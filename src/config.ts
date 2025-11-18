44396b44715549d0a05d8127380dd20d69c5a062
The Quan Dao <tqan876@gmail.com>
2025-11-18 23:48:07 +0700

    update

--- src/config.ts
+++ src/config.ts
@@ -44,11 +44,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		{
-			name: "My Fam",
-			url: "https://0x0r3n.github.io/", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		},
+		// {
+		// 	name: "My Fam",
+		// 	url: "https://0x0r3n.github.io/", // Internal links should not include the base path, as it is automatically added
+		// 	external: true, // Show an external link icon and will open in a new tab
+		// },
 	],
 };
 
